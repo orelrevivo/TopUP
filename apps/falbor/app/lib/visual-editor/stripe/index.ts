483b211228095bdfd5eb452a48957dfe17856945
orelrevivo <orelrevivo3999@gmail.com>
2026-08-15 11:26:17 +0300

    chore: comment out stripe apiVersion configuration

--- apps/falbor/app/lib/visual-editor/stripe/index.ts
+++ apps/falbor/app/lib/visual-editor/stripe/index.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_123', {
-  apiVersion: '2023-10-16',
+  //apiVersion: '2023-10-16',
   appInfo: {
     name: 'Plura App',
     version: '0.1.0',
