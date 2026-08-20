9b1e2bb98f1b5828f85d17041bf5129fb2dc696d
orelrevivo <orelrevivo3999@gmail.com>
2026-08-20 10:54:11 +0300

    fix: use placeholder stripe key to prevent build crash

--- apps/falbor/app/lib/visual-editor/stripe/index.ts
+++ apps/falbor/app/lib/visual-editor/stripe/index.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'STRIPE_SECRET_KEY_PLACEHOLDER', {
   //apiVersion: '2023-10-16',
   appInfo: {
     name: 'Plura App',
