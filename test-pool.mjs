7ad47a3776a336ad9513973fe38a3b280f8c0d2c
orelrevivo <orelrevivo3999@gmail.com>
2026-08-23 10:37:02 +0300

    Update

--- test-pool.mjs
+++ test-pool.mjs
@@ -1,5 +1,5 @@
 import { Pool } from '@neondatabase/serverless';
-const pool = new Pool({ connectionString: 'postgresql://neondb_owner:npg_Uj32HluyYoWS@ep-green-glade-axmu503k-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require' });
+const pool = new Pool({ connectionString: 'Enter your connection string here' });
 try {
   const res = await pool.query('SELECT 1 as x');
   console.log('Success:', res.rows);
